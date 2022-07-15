e92b9f7972b838c62db6c5719145c3a638c54b8a
午华彬 <18334786948@163.com>
2022-07-15 18:28:16 +0800

    引入 viewbinding 库

--- app/src/main/java/com/wuhuabin/cookbook/ui/MainActivity.kt
+++ app/src/main/java/com/wuhuabin/cookbook/ui/MainActivity.kt
@@ -4,23 +4,23 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.dylanc.viewbinding.binding
 import com.wuhuabin.common.base.BaseActivity
 import com.wuhuabin.cookbook.R
 import com.wuhuabin.cookbook.databinding.ActivityMainBinding
 import com.wuhuabin.cookbook.viewmodel.UserViewModel
 
 class MainActivity : BaseActivity() {
+    private val binding: ActivityMainBinding by binding()
     val userViewModel: UserViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val inflate = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(inflate.root)
-
+        setContentView(binding.root)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
 
         val navController = navHostFragment.navController
-        inflate.bottomNavigationView.setupWithNavController(navController)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
